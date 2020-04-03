55b3bf07927f62a5e5fad993d06333473fa92bbd
FAHansel <62783826+FAHansel@users.noreply.github.com>
2020-04-03 13:28:02 -0300

    Update NP_F1_F2_.R

--- Nitrogen/NP_F1_F2_.R
+++ Nitrogen/NP_F1_F2_.R
@@ -12,7 +12,7 @@ head(data);names(data);dim(data); str(data);summary(data);
 
 ###NON PARAMETRIC STATS GIVE F2 AND F1 SIGNIFICANT
 
-###CHECK THE F2 "dad_L1, dad_L2, dad_L3, dad_L4 and dad_L5" LEVELS FOR F1 SIGNIFICANT - "IVA10 and IVA34" - NOTE: REPLACE THE COMPOUNDS ABBREVIATION ON THE SCRIPT
+###CHECK THE F2 "dad_L1, dad_L2, dad_L3, dad_L4 and dad_L5" LEVELS FOR F1 SIGNIFICANT - "EC22 and EC40" - NOTE: REPLACE THE COMPOUNDS ABBREVIATION ON THE SCRIPT
 
 ###CHECK THE F1 "dad_L1 and dad_L2" LEVELS FOR F2 SIGNIFICANT - "NITROGEN DOSES" - NOTE: REPLACE THE COMPOUNDS ABBREVIATION ON THE SCRIPT
 
@@ -39,11 +39,11 @@ dad_L5=with(data, subset(data, data$F2=="1142"))
 kruskal.test(C28~F1, data = dad_L5)
 comparison<-kruskal(dad_L5$C28,dad_L5$F1,group=TRUE);comparison
 
-dad_L1=with(data, subset(data,data$F1=="IVA10"))
+dad_L1=with(data, subset(data,data$F1=="EC22"))
 kruskal.test(C28~F2, data = dad_L1)
 comparison<-kruskal(dad_L1$C28,dad_L1$F2,group=TRUE);comparison
 
-dad_L2=with(data, subset(data,data$F1=="IVA34"))
+dad_L2=with(data, subset(data,data$F1=="EC40"))
 kruskal.test(C28~F2, data = dad_L2)
 comparison<-kruskal(dad_L2$C28,dad_L2$F2,group=TRUE);comparison
 
