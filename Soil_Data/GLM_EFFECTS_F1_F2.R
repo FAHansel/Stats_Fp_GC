85abd7a6f39f3898cb97514a42d56b5ea48968f4
FAHansel <62783826+FAHansel@users.noreply.github.com>
2020-04-20 16:28:12 -0300

    Update GLM_EFFECTS_F1_F2.R

--- Soil_Data/GLM_EFFECTS_F1_F2.R
+++ Soil_Data/GLM_EFFECTS_F1_F2.R
@@ -14,23 +14,17 @@ summary(subset(data,data$F1=="L2"&data$F2=="L1"));summary(subset(data,data$F1=="
 
 ###RUN THE GLM STATS
 
-m_C53=glm(C53~F1/F2,family=Gamma, data=data)
-anova(m_C53,test="Chisq"); 
-par(mfrow=c(2,2));plot(m_C53)
-
 ###SEE INTO THE LEVELS
 
 ###F1
 
 m_C53=glm(C53~F1,family=Gamma, subset(data,data$F2=="L1"))
-anova(m_C53,test="Chisq");
 par(mfrow=c(2,2));plot(m_C53)
 comp= summary(glht(m_C53, linfct=mcp(F1="Tukey")), test=adjusted(type="single-step"))
 comp
 let=cld(comp,decreasing = TRUE);let
 
 m_C53=glm(C53~F1,family=Gamma, subset(data,data$F2=="L2"))
-anova(m_C53,test="Chisq");
 par(mfrow=c(2,2));plot(m_C53)
 comp= summary(glht(m_C53, linfct=mcp(F1="Tukey")), test=adjusted(type="single-step"))
 comp
@@ -38,7 +32,6 @@ let=cld(comp,decreasing = TRUE);let
 
 
 m_C53=glm(C53~F1,family=Gamma, subset(data,data$F2=="L3"))
-anova(m_C53,test="Chisq");
 par(mfrow=c(2,2));plot(m_C53)
 comp= summary(glht(m_C53, linfct=mcp(F1="Tukey")), test=adjusted(type="single-step"))
 comp
@@ -47,14 +40,12 @@ let=cld(comp,decreasing = TRUE);let
 ###F2
 
 m_C53=glm(C53~F2,family=Gamma, subset(data,data$F1=="L1"))
-anova(m_C53,test="Chisq");
 par(mfrow=c(2,2));plot(m_C53)
 comp= summary(glht(m_C53, linfct=mcp(F2="Tukey")), test=adjusted(type="single-step"))
 comp
 let=cld(comp,decreasing = TRUE);let
 
 m_C53=glm(C53~F2,family=Gamma, subset(data,data$F1=="L2"))
-anova(m_C53,test="Chisq"); 
 par(mfrow=c(2,2));plot(m_C53)
 comp= summary(glht(m_C53, linfct=mcp(F2="Tukey")), test=adjusted(type="single-step"))
 comp
