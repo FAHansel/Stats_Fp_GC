513e0952504f7f1ef7c49caf32279ed07ffc6b9c
FAHansel <62783826+FAHansel@users.noreply.github.com>
2020-04-20 16:47:56 -0300

    Update COMP_GLM_F1.R

--- Nitrogen/COMP_GLM_F1.R
+++ Nitrogen/COMP_GLM_F1.R
@@ -13,7 +13,6 @@ head(data);names(data);dim(data); str(data);summary(data);
 ###RUN THE GLM COMPARISON WITH CLONE SIGNIFICANT - NOTE: REPLACE THE COMPOUNDS ABBREVIATION ON THE SCRIPT
 
 m_C29=glm(C29~F1,family=Gamma,data=data) 
-anova(m_C29,test="Chisq"); 
 par(mfrow=c(2,2));plot(m_C29)
 hnp(m_C29, pch = 20, cex = 1.2)
 comp= summary(glht(m_C29, linfct=mcp(F1="Tukey")), test=adjusted(type="single-step"))
